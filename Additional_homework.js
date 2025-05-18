00891824b93d109d7f2a5d19cb014bb2db048003
Yuliia Bilichenko <uliabelichenko@gmail.com>
2025-05-18 21:34:06 +0300

    finished homework

--- Additional_homework.js
+++ Additional_homework.js
@@ -54,3 +54,98 @@ let booksLeft = booksGoal - booksRead;
 console.log(
   "I need to read " + booksLeft + " more books to reach my goal of " + booksGoal
 );
+/* Task - 8 */
+let input = "42";
+let convertedInput = Number(input);
+console.log(convertedInput);
+let unaryConverted = +42;
+console.log(typeof input, typeof convertedInput, typeof unaryConverted);
+
+let multInput = input * 2;
+console.log(multInput, typeof multInput);
+// Отримали 84 number бо відбулося неявне перетворення змінної input з типом string у тип number.
+
+/* Task - 9; */
+let counter = 10;
+console.log(counter++);
+console.log(++counter);
+/* Постфікстний інкремент спочатку прирівнює до значення змінної, а подальші буде додавати 1. Префікстний інкремент до значення зсінної додає 1, а потім прирівнює; */
+let result = counter++;
+console.log(result);
+console.log(counter++);
+/* Останнє значення змінної counter = 12, після цього ми прирівняли до змінної result, тому result = 12. Після цього +1 і тому вже counter = 13. */
+
+/* Task - 10 */
+let x = 3;
+let y = 4;
+let z = 0;
+
+let digits = x + y + "" + z;
+
+console.log(digits);
+
+/* Task - 11 */
+let bigAmount = "500";
+let greatAmount = "100000";
+
+let hugeAmount = Number(bigAmount) + Number(greatAmount); //change this line
+
+console.log(hugeAmount);
+
+/* Task - 12; */
+
+let emptyString = "";
+
+console.log(emptyString.length);
+console.log("Think Different".length);
+console.log(emptyString + 2 + 2 + "22".length);
+
+/* Task - 13 */
+
+let title = "Senior Lead Principal Data Analyst";
+let degree =
+  "In college, I Majored in Economics Science and Minored in Musician Studies.";
+let career =
+  "Data Scientist With 3+ Years of Experience in Big Data Analytics.";
+
+console.log("RESUME".toLowerCase());
+console.log("TITLE: ".toLowerCase() + title);
+console.log("DEGREE: ".toLowerCase() + degree);
+console.log("CAREER: ".toLowerCase() + career);
+
+/* Task - 14 */
+
+let caps = "if I type in caps ";
+let fbi = "фбр";
+
+console.log(fbi.toUpperCase());
+console.log("Гекльберрі Фінн".toUpperCase());
+console.log(caps.toUpperCase() + "they know I mean business".toUpperCase());
+
+/* Task - 15  */
+
+let inputX = 2;
+let inputY = 3;
+
+let resultX = Number(inputX);
+let resultY = Number(inputY);
+
+let sum = inputX + inputY;
+let multiply = inputX * inputY;
+let division = inputY / inputX;
+let subtraction = inputY - inputX;
+
+console.log(`${resultX} + ${resultY} = ${sum}`);
+console.log(`${resultX} * ${resultY} = ${multiply}`);
+console.log(`${resultY} / ${resultX} = ${division}`);
+console.log(`${resultY} - ${resultX} = ${subtraction}`);
+
+/* Task - 16 */
+
+let name1 = "Максим";
+let age1 = 29;
+let company = "Apple";
+let position = "QA";
+
+let message = `Привіт, мене звати ${name1} .  Мені ${age1} років. Я працюю на посаді ${position} в компанії ${company}`;
+console.log(message);
